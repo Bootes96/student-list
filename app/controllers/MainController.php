328b425b200598afc13e3ed4bdc4ab149ab37355
Bootes96 <andreyantman@gmail.com>
2023-07-04 19:34:53 +0300

    Добавил пагинацию

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -2,10 +2,16 @@
 
 namespace app\controllers;
 
+use app\helpers\Pagination;
 
 class MainController extends AppController{
     public function indexAction() {
-        $users = \R::findAll('user');
-        $this->set(compact('users'));
+        $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+        $perPage = 5;
+        $total = \R::count('user');
+        $pagination = new Pagination($page, $perPage, $total); 
+        $start = $pagination->getStart();
+        $users = \R::findAll('user', "LIMIT $start, $perPage");
+        $this->set(compact('users', 'pagination'));
     }
 }
--- app/views/Main/index.php
+++ app/views/Main/index.php
@@ -28,28 +28,10 @@
     </div>
     </a>
     <?php endforeach;?>
+    <div class="text-center">
+        <?php if($pagination->countPages > 1): ?>
+            <?=$pagination;?>
+        <?php endif;?>
+    </div>
 </div>
-  <?php endif;?>
-<div class="pagination">
-    <nav aria-label="Page navigation example">
-        <ul class="pagination">
-            <li class="page-item">
-                <a class="page-link" href="#" aria-label="Previous">
-                    <span aria-hidden="true">&laquo;</span>
-                </a>
-            </li>
-            <li class="page-item"><a class="page-link" href="#">1</a></li>
-            <li class="page-item"><a class="page-link" href="#">2</a></li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-            <li class="page-item"><a class="page-link" href="#">4</a></li>
-            <li class="page-item"><a class="page-link" href="#">5</a></li>
-            <li class="page-item"><a class="page-link" href="#">6</a></li>
-            <li class="page-item"><a class="page-link" href="#">Последняя</a></li>
-            <li class="page-item">
-                <a class="page-link" href="#" aria-label="Next">
-                    <span aria-hidden="true">&raquo;</span>
-                </a>
-            </li>
-        </ul>
-    </nav>
-</div>
+<?php endif;?>
